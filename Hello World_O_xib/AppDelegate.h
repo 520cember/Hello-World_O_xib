f6da227cf9f53ba198d721a855b5d1043a886187
520cember <1029278668@qq.com>
2016-01-13 01:50:15 +0800

    Initial Commit

---
+++ Hello World_O_xib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World_O_xib
+//
+//  Created by 董兴斌 on 16/1/13.
+//  Copyright © 2016年 董兴斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World_O_xibTests/Hello_World_O_xibTests.m
@@ -0,0 +1,39 @@
+//
+//  Hello_World_O_xibTests.m
+//  Hello World_O_xibTests
+//
+//  Created by 董兴斌 on 16/1/13.
+//  Copyright © 2016年 董兴斌. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hello_World_O_xibTests : XCTestCase
+
+@end
+
+@implementation Hello_World_O_xibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
